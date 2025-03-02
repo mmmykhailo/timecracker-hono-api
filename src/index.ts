cd00bc94b10437811faccb4faa1fbd81c061f8a0
mmmykhailo <mmmykhailo@proton.me>
2025-03-02 16:19:51 +0200

    add scalar docs

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,51 @@
-import { Hono } from 'hono'
+import { OpenAPIHono, createRoute, z } from "@hono/zod-openapi";
+import { apiReference } from "@scalar/hono-api-reference";
 
-const app = new Hono()
+const app = new OpenAPIHono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.openapi(
+	createRoute({
+		tags: ["Index", "not index"],
+		method: "get",
+		path: "/",
+		responses: {
+			200: {
+				description: "Respond a message",
+				content: {
+					"application/json": {
+						schema: z.object({
+							message: z.string(),
+						}),
+					},
+				},
+			},
+		},
+	}),
+	(c) => {
+		return c.json({
+			message: "hello",
+		});
+	},
+);
 
-export default { 
-  port: 3000, 
-  fetch: app.fetch, 
-} 
+app.get(
+	"/ref",
+	apiReference({
+		layout: "classic",
+		pageTitle: "Timecracker API Reference",
+		spec: { url: "/doc" },
+	}),
+);
+
+app.doc("/doc", {
+	info: {
+		title: "An API",
+		version: "v1",
+	},
+	openapi: "3.1.0",
+});
+
+export default {
+	port: 3000,
+	fetch: app.fetch,
+};
